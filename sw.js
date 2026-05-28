f98264a2bd031612b21d5f6efa83a189b2dbfb17
semb-commits <sembsui@gmail.com>
2026-05-28 15:05:16 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,25 @@
-const CACHE_NAME = "admin-cache-v1";
+const CACHE_NAME = "admin-cache-v2";
 const urlsToCache = [
   "/admin.html",
-  "/favicon.png"
+  "/manifest.json",
+  "/icon.png",
+  "/icon-512-1.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME)
+        .map((name) => caches.delete(name))
+      );
+    })
   );
 });
 
